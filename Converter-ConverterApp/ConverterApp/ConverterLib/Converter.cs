a04795b879588583a528ea920fc7991501ed5ad5
unh4ppy <116509658+unh4ppy@users.noreply.github.com>
2022-12-16 20:38:10 +0300

    Add files via upload

---
+++ Converter-ConverterApp/ConverterApp/ConverterLib/Converter.cs
@@ -0,0 +1,83 @@
+﻿using ConverterLib.Values;
+using System.Reflection;
+
+namespace ConverterLib
+{
+    public class Converter
+    {
+        public Converter()
+        {
+            SetValuesList();
+        }
+        /// <summary>
+        /// Список физических величин
+        /// </summary>
+        private static List<IValue> _physicValues = new List<IValue>();
+        
+        private static void SetValuesList()
+        {
+            Assembly asm = Assembly.LoadFrom("ValuesLib.dll");
+            Type[] types = asm.GetTypes();
+            foreach (Type type in types)
+            {
+                if ((type.IsInterface == false) && (type.IsAbstract == false) && (type.GetInterface("IValue") != null))
+                {
+                    IValue value = (IValue)Activator.CreateInstance(type);
+                    _physicValues.Add(value);
+                }
+            }
+        }
+        /// <summary>
+        /// Метод возвращает список физических величин, реализованных в приложении
+        /// </summary>
+        /// <returns>Списко физических величин</returns>
+        public List<string> GetPhysicValuesList()
+        {
+            List<string> physicValueList = new List<string>();
+            foreach (IValue value in _physicValues)
+            {
+                physicValueList.Add(value.GetValueName());
+            }
+            return physicValueList;
+        }
+        private void SetIValue(string valueName)
+        {
+            foreach (var value in _physicValues)
+            {
+                if (value.GetValueName() == valueName)
+                {
+                    _value = value;
+                }
+                if (_value == null)
+                {
+                    throw new Exception("Ошибка! В библиотеки нет такой величины");
+                }
+            }
+        }
+        /// <summary>
+        /// Метод возвращает список единиц измерения
+        /// </summary>
+        /// <param name="physicValue">Наименовани физической величены</param>
+        /// <returns>список единиц измерения</returns>
+        public List<string> GetMeassureList(string physicValue)
+        {
+            SetIValue(physicValue);
+            return _value.GetMeassureList();
+        }
+        private IValue _value;
+        /// <summary>
+        /// Возвращает конвертированное значение
+        /// </summary>
+        /// <param name="physicValue">Физическая величина</param>
+        /// <param name="value">Значение</param>
+        /// <param name="from">Из каких ед измерения</param>
+        /// <param name="to">в какие ед измерения</param>
+        /// <returns>конвертированное значение</returns>
+        public double GetConvertedValue(string physicValue,double value, string from, string to)
+        {
+            
+            return _value.GetConvertedValue(physicValue, value, from, to); 
+        }
+       
+    }
+}
---
+++ Converter-ConverterApp/ConverterApp/ValuesLib/Time.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConverterLib.Values
+{
+    public class Time : IValue
+    {
+        /// <summary>
+        /// Список единиц измерения
+        /// </summary>
+        private List<string> _measureList = new List<string>()
+        {
+            "Секунды",
+            "Минуты",
+            "Часы",
+            "Милисекунды"
+        };
+        private string _valueName = "Время";
+        private double _value;
+        private string _from;
+        private string _to;
+
+     /// <summary>
+     /// Вщзвращает конвертированное значение
+     /// </summary>
+     /// <param name="physicValue">Физическая величина</param>
+     /// <param name="value">значение</param>
+     /// <param name="from">из каких ед измерения</param>
+     /// <param name="to">в какие ед измерения</param>
+     /// <returns>конвертированное значение</returns>
+        public double GetConvertedValue(string physicValue, double value, string from, string to)
+        {
+            foreach (string item in _measureList)
+            {
+                if (item == from || item == to)
+                {
+                    _value = value;
+                    _from = from;
+                    _to = to;
+                    ToSI();
+                    ToRequired();
+                    return _value;
+
+                }
+            }
+
+            return 00000;
+        }
+        /// <summary>
+        /// Перевод в СИ
+        /// </summary>
+        public void ToSI()
+        {
+            switch(_from)
+            {
+                case "Минуты":
+                    _value *= 60;
+                    break;
+
+                case "Часы":
+                    _value *= 3600;
+                    break;
+
+                case "Милисекунды":
+                    _value /=1000 ;
+                    break;
+
+                case "Секунды":
+                    _value = _value;
+                    break;
+                
+            }
+                
+         
+            
+        }
+        /// <summary>
+        /// Перевод в необходимые ед измерения
+        /// </summary>
+        public void ToRequired()
+        {
+            //преобразовать в необходимое
+            switch (_to)
+            {
+                case "Минуты":
+                    _value /= 60;
+                    break;
+                case "Часы":
+                    _value /= 3600;
+                    break;
+
+                case "Милисекунды":
+                    _value *= 1000;
+                    break;
+                case "Cекунды":
+                    _value += 0;
+                    break;
+
+            }
+            
+        }
+        /// <summary>
+        /// Возвращает список единиц измерения
+        /// </summary>
+        /// <returns>Список единиц измерения</returns>
+        public List<string> GetMeassureList()
+        {
+            return _measureList;
+        }
+        public string GetValueName()
+        {
+            return _valueName;
+        }
+    }
+}
